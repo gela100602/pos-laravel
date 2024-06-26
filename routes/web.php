10c13a02535df8a048c86e5e77428e502af2675d
Angela Arguelles <angelaarguelles04@gmail.com>
2024-06-27 06:17:22 +0800

    Routes

--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,22 @@
 <?php
 
-use App\Http\Controllers\{
-    DashboardController,
-    CategoryController,
-    DiscountController,
-    PaymentMethodController,
-    SupplierController,
-    CustomerController,
-    ProductController,
-    UserController,
-};
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DiscountController;
+use App\Http\Controllers\PaymentMethodController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Auth\LoginController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Automatically define authentication routes
+Auth::routes();
 
-// Route::get('/', function () {
-//     return view('admin.dashboard');
-// });
-
-
-Route::get('/', function () {
-    return redirect()->route('login');
-});
-
-Route::group(['middleware' => 'auth'], function () {
+Route::group(['middleware' => ['web', 'auth']], function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
 
     Route::get('/category/data', [CategoryController::class, 'data'])->name('category.data');
     Route::resource('/category', CategoryController::class);
@@ -55,15 +36,19 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/products/data', [ProductController::class, 'data'])->name('products.data');
     Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
     Route::post('/products/delete-selected', [ProductController::class, 'deleteSelected'])->name('products.delete_selected');
-    Route::resource('/products', ProductController::class); 
+    Route::resource('/products', ProductController::class);
 
     Route::get('/users/data', [UserController::class, 'data'])->name('users.data');
     Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
     Route::post('/users/delete-selected', [UserController::class, 'deleteSelected'])->name('users.delete_selected');
     Route::resource('/users', UserController::class);
 
-    // Route::get('/user/data', [UserController::class, 'data'])->name('user.data');
-    // Route::resource('/user', UserController::class);
     Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+});
 
-    });
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Redirect root to login if not authenticated
+Route::get('/', function () {
+    return redirect()->route('login');
+})->name('index');
